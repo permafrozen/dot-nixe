101de534efd3fd12d08ad2cda9acad2f4a7b9ff1
Permafrozen <m8tt8o@gmail.com>
2024-09-19 19:08:34 +0200

    flake manages configuration.nix module now

--- flake.nix
+++ flake.nix
@@ -1,1 +1,16 @@
-# Dummy
+{
+  description = "Main NixOS configuration Flake";
+
+  inpurts  = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {  }:
+
+  {
+    nixosConfigurations.joes-desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configs/common/configuration.nix ];
+    };
+  };
+}
