e8c8ca5f244c3f87e588fe8be4e594a14494cfb6
Permafrozen <permafrozen@disroot.org>
2024-09-19 21:24:59 +0200

    added home-manager to flake + cleanup

--- flake.nix
+++ flake.nix
@@ -2,16 +2,30 @@
   description = "Main NixOS configuration Flake";
 
   inputs  = {
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@Inputs:
-
-  {
-    nixosConfigurations.joes-desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configs/common/configuration.nix ];
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.matteo = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [ 
+        ./configs/common/configuration.nix 
+        home-manager.nixosModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs; };
+            users.matteo = import ./configs/common/home.nix;
+          };
+        }
+      ];
     };
   };
 }
