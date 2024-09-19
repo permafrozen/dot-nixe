cb59b8eda6fd746a1914d103f116bd5d04b30e46
Permafrozen <permafrozen@disroot.org>
2024-09-19 20:27:00 +0200

    added home.nix config

--- configs/common/home.nix
+++ configs/common/home.nix
@@ -1,1 +1,14 @@
-# dummy
+{ inputs, config, pkgs, ... }:
+
+{
+  # Information what to manage
+  home = {
+    username = vars.username;
+    homeDirectory = "/home/matteo";
+    stateVersion = "24.05";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+ 
+}
