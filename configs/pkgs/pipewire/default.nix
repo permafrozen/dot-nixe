bd9fce20dc73de5e6106f37a9312573cfa6e62ed
Permafrozen <permafrozen@disroot.org>
2024-09-20 11:15:17 +0200

    added piepewire to configs/pkgs

---
+++ configs/pkgs/pipewire/default.nix
@@ -0,0 +1,26 @@
+{ config, pgks, ...}:
+
+{
+  # Pipewire
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-camera" = {
+          "wireplumber.profiles" = {
+            main."monitor.libcamera" = "disabled";
+          };
+        };
+      };
+    };
+  };
+
+  # Enable the user-level service manager to start PipeWire automatically
+  systemd.user.services = {
+    pipewire.wantedBy = [ "default.target" ];
+    pipewire-pulse.wantedBy = [ "default.target" ];
+  };
+}
