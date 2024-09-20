049022145a90a7f7b8b55a9f9b621a5828b962b2
Permafrozen <m8tt8o@gmail.com>
2024-09-20 20:20:50 +0200

    hyprland workspace keybinds

--- configs/pkgs/hyprland/default.nix
+++ configs/pkgs/hyprland/default.nix
@@ -90,8 +90,38 @@
         };
 
         bind = [
+          # Important Keybinds
           "$mainMod, T, exec, kitty"
           "$mainMod, Q, killactive,"
+          "$mainMod, M, exit,"
+
+          # Cursor resize/move
+          "$mainMod, mouse:272, movewindow"
+          "$mainMod, mouse:273, resizewindow"
+
+          # move to workspace
+          "$mainMod, 1, workspace, 1"
+          "$mainMod, 2, workspace, 2"
+          "$mainMod, 3, workspace, 3"
+          "$mainMod, 4, workspace, 4"
+          "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
+          "$mainMod, 0, workspace, 10"
+
+          # move active window to workspace
+          "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+          "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+          "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+          "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+          "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+          "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+          "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+          "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+          "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+          "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
         ];
 
         input = {
