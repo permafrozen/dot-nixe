410957f75b97df7b6ceab9206a05f21ee782459b
Permafrozen <permafrozen@disroot.org>
2024-09-20 20:25:37 +0200

    removed comments

--- configs/pkgs/hyprland/default.nix
+++ configs/pkgs/hyprland/default.nix
@@ -78,14 +78,14 @@
             "windowsMove, 1, 2, easeinoutsine, slide" # everything in between, moving, dragging, resizing.
 
             # Fade
-            "fadeIn, 1, 3, easeOutCubic" # fade in (open) -> layers and windows
-            "fadeOut, 1, 2, easeOutCubic" # fade out (close) -> layers and windows
-            "fadeSwitch, 0, 1, easeOutCirc" # fade on changing activewindow and its opacity
-            "fadeShadow, 1, 10, easeOutCirc" # fade on changing activewindow for shadows
-            "fadeDim, 1, 4, fluent_decel" # the easing of the dimming of inactive windows
-            "border, 1, 2.7, easeOutCirc" # for animating the border's color switch speed
-            "borderangle, 1, 30, fluent_decel, once" # for animating the border's gradient angle - styles: once (default), loop
-            "workspaces, 1, 4, easeOutCubic, fade" # styles: slide, slidevert, fade, slidefade, slidefadevert
+            "fadeIn, 1, 3, easeOutCubic"
+            "fadeOut, 1, 2, easeOutCubic"
+            "fadeSwitch, 0, 1, easeOutCirc"
+            "fadeShadow, 1, 10, easeOutCirc" 
+            "fadeDim, 1, 4, fluent_decel" 
+            "border, 1, 2.7, easeOutCirc"
+            "borderangle, 1, 30, fluent_decel, once" 
+            "workspaces, 1, 4, easeOutCubic, fade"           
           ];
         };
 
