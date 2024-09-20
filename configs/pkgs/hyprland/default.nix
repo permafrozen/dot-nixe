36aed9eef3c537db3e6162b72d2f2fff48e579c0
Permafrozen <m8tt8o@gmail.com>
2024-09-20 12:03:31 +0200

    added hyprland home-manager config, to tests out the syntax

--- configs/pkgs/hyprland/default.nix
+++ configs/pkgs/hyprland/default.nix
@@ -1,5 +1,28 @@
 { configs, pkgs, settings, ...}:
 
 {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
+  environment.systemPackages = with pkgs; [
+    hyprshot # for screenshots
+  ];
+
+  home-manager.sharedModules = [{
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        general = {
+          border_size = "1";
+          gaps_in = "5";
+          gaps_out = "5";
+          "col.inactive_border" = "0xffFFFFFF";
+          "col.active_border" = "0xffFFFFFF";
+          layout = "dwindle";
+        };
+      };
+    };
+  }];
 }
