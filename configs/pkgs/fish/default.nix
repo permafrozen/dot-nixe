dda2cb85535663ae8ebb0e4aefcf43164e4d8af0
Permafrozen <m8tt8o@gmail.com>
2024-09-22 20:59:45 +0200

    added fish config

---
+++ configs/pkgs/fish/default.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, settings, ...}:
+
+{
+  home-manager.sharedModules = [{
+    programs.fish = {
+      shellInit = ''
+        set -U fish_greeting
+        cd
+      '';
+    };
+  }];
+}
