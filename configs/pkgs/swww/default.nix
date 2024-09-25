ee8b553dca00c198f2ed13ae2a5d87aeaf8c450d
Permafrozen <m8tt8o@gmail.com>
2024-09-25 18:20:54 +0200

    removed unneccessary commented code from swww

--- configs/pkgs/swww/default.nix
+++ configs/pkgs/swww/default.nix
@@ -5,17 +5,6 @@
     swww
   ];
 
-  # systemd.services.swww-daemon = {
-  #   description = "Swww Daemon";
-  #   wantedBy = [ "multi-user.target" ];
-  #   after = [ "network.target" ]; # Adjust as needed
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.swww}/bin/swww-daemon";
-  #     Restart = "always";
-  #     User = "${settings.userName}";
-  #   };
-  # };
-
   home-manager.sharedModules = [{
     wayland.windowManager.hyprland.extraConfig = ''
       exec-once = swww-daemon
