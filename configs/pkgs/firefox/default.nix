d2650c32a9ad54b665b1feb222821dcfb6bd1102
Permafrozen <permafrozen@disroot.org>
2024-09-27 09:49:37 +0200

    updated firefox Configuration

--- configs/pkgs/firefox/default.nix
+++ configs/pkgs/firefox/default.nix
@@ -7,6 +7,22 @@
       profiles.default = {
         id = 0;
         name = "default";
+        isDefault = true;
+        settings = {
+          "browser.startup.homepage" = "https://nixos.org";
+          "browser.search.region" = "AT";
+          "browser.search.isUS" = false;
+          "distribution.searchplugins.defaultLocale" = "de_AT";
+          "general.useragent.locale" = "de_AT";
+          "browser.bookmarks.showMobileBookmarks" = false;
+          "browser.newtabpage.pinned" = [{
+            title = "NixOS";
+            url = "https://nixos.org";
+          }];
+        };
+        userChrome = {
+
+        };
       };
     };
   }];
