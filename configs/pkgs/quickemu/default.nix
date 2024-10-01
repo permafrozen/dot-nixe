85eb87963cb5006a0d875836d40c4acf16e9f9cf
Permafrozen <permafrozen@disroot.org>
2024-10-01 09:56:04 +0200

    Added quickemu
    Quickemu config availabe in pkgs

---
+++ configs/pkgs/quickemu/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, settings, ...}:
+
+{
+  environment.systemPackages = with pkgs; [
+    quickemu
+  ];
+
+  # Enable virt-manager and dconf
+  programs.virt-manager.enable = true;
+  programs.dconf.enable = true;
+
+  # Enable libvirtd
+  virtualisation.libvirtd = {
+    enable = true;
+  };
+
+  # Ensure user is added to the libvirtd group
+  users.users.${settings.userName}.extraGroups = [ "libvirtd" ];
+
+  # Kernel module and virtualization settings specific to AMD
+  boot.kernelModules = [ "kvm" "kvm-amd" ];  # Use "kvm-amd" for AMD CPUs
+  boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ]; # Enable IOMMU for AMD
+}
