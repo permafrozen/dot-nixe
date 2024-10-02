b127a77fe9b05a95876253d77df648cf97219af2
Permafrozen <permafrozen@disroot.org>
2024-10-02 11:03:20 +0200

    updated synthing config
    Removed unnecessary parts, working now.

--- configs/pkgs/syncthing/default.nix
+++ configs/pkgs/syncthing/default.nix
@@ -2,13 +2,27 @@
 
 {
   services = {
-    syncthing = {
-        enable = true;
-        user = "${settings.userName}";
-        dataDir = "/home/${settings.userName}/Documents/Synced";    # Default folder for new synced folders
-        configDir = "/home/${settings.userName}/Documents/Synced/.config/syncthing";   # Folder for Syncthing's settings and keys
+  syncthing = {
+    enable = true;
+    user = "${settings.userName}";
+    dataDir = "/home/${settings.userName}/Documents/Synced";
+    configDir = "/home/${settings.userName}/Documents/Synced/.config/syncthing";
+    overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+    overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+    settings = {
+      devices = {
+        "josef-laptop" = { id = "NGXEO2S-C3RGM4X-R4FLDPO-DBWVZHJ-OZ46FL3-GYE5XRH-PUYPXEX-7MOPHQI"; };
+      };
+      folders = {
+        "Documents/Synced" = {                    # Folder ID in Syncthing, also the name of folder (label) by default
+          label = "Schule";
+          path = "/home/${settings.userName}/Documents/Synced";    # Which folder to add to Syncthing
+          devices = [ "josef-laptop" ];      # Which devices to share the folder with
+        };
+      };
     };
   };
+};
 
   # Disable default folder
   systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
