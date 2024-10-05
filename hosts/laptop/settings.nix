233537d569acd19ae7b21c8f1ccd516da60e1813
Permafrozen <permafrozen@disroot.org>
2024-10-05 16:41:43 +0200

    updated settings.nix
    Reformated the file to look pretty ><

--- hosts/laptop/settings.nix
+++ hosts/laptop/settings.nix
@@ -1,32 +1,32 @@
 {
   # system settings
-  userName =    "matteo";             # foo@...
-  hostName =    "nixos";              # ...@bar
-  hostPreset =  "laptop";             # Name of the host config to use (this one is in /laptop)
+  userName =      "matteo";             # foo@...
+  hostName =      "nixos";              # ...@bar
+  hostPreset =    "laptop";             # Name of the host config to use (this one is in /laptop)
 
   # local specifics
-  timeZone =    "Europe/Vienna";      # system time
-  kbLayout =    "de";                 # Be careful, some layouts are not supported, e.g. "at" for Austria
-  locale =      "de_AT";              # I think this one explains itself
-  searchRegion = "AT";                # Region for browsing purposes
+  timeZone =      "Europe/Vienna";      # system time
+  kbLayout =      "de";                 # Be careful, some layouts are not supported, e.g. "at" for Austria
+  locale =        "de_AT";              # I think this one explains itself
+  searchRegion =  "AT";                 # Region for browsing purposes
 
   # git settings
-  gitUser = "Permafrozen";
-  gitMail = "m8tt8o@gmail.com";
+  gitUser =       "Permafrozen";
+  gitMail =       "m8tt8o@gmail.com";
   defaultBranch = "main";
 
   # theme
-  wallpaper =   "space.png";          # background image, located in .../assets/wallpaper/ , also write the file ending!
-  scheme =      "eris";               # Color Theme (Chose any from the base16-schemes Package)
+  wallpaper =     "space.png";          # background image, located in .../assets/wallpaper/ , also write the file ending!
+  scheme =        "eris";               # Color Theme (Chose any from the base16-schemes Package)
 
   # hyprland
-  rounding =    "10";                 # Corner rounding
-  gaps =        "10";                 # Gaps between windows
-  shadow =      "true";               # Wheater to enable window shadow
-  opacity =     "0.7";                # background opacities for supported windows
-  blur =        "7";                  # background blur for supported windows
+  rounding =      "10";                 # Corner rounding
+  gaps =          "10";                 # Gaps between windows
+  shadow =        "true";               # Wheater to enable window shadow
+  opacity =       "0.7";                # background opacities for supported windows
+  blur =          "7";                  # background blur for supported windows
 
   # default apps
-  browser = "firefox";                # only firefox for now
-  terminal = "kitty";                 # only kitty for now
+  browser =       "firefox";            # only firefox for now
+  terminal =      "kitty";              # only kitty for now
 }
