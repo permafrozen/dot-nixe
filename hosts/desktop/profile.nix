82059d8b7c5676ac9231d236bb9f80673cc27e34
Permafrozen <permafrozen@disroot.org>
2024-10-08 16:30:18 +0200

    updated desktop
    now actually using the config for my desktop pc ;)

---
+++ hosts/desktop/profile.nix
@@ -0,0 +1,34 @@
+{ settings, ... }:
+  let
+    getPath = folderName: ../../configs/pkgs/${folderName}/default.nix;
+  in 
+{
+  imports = [
+    # Normal Apps
+    (getPath "pipewire")
+    (getPath "hyprland")
+    (getPath "fish")
+    (getPath "starship")
+    (getPath "stylix")
+    (getPath "swww")
+    (getPath "ly")
+    (getPath "lazygit")
+    (getPath "vscode")
+    (getPath "libreoffice")
+    (getPath "yazi")
+    (getPath "bottom")
+    (getPath "obsidian")
+    # (getPath "quickemu")
+    (getPath "vesktop")
+    (getPath "git")
+    # (getPath "syncthing")
+    (getPath "ani-cli")
+
+    # GPU Settings
+    (getPath "nvidia")
+
+    # Variable Apps
+    (getPath "${settings.terminal}")  # BROWSER
+    (getPath "${settings.browser}")   # TERMINAL
+  ];
+}
--- hosts/desktop/settings.nix
+++ hosts/desktop/settings.nix
@@ -1,1 +1,35 @@
-# Dummy
+{
+  # system settings
+  userName =      "matteo";             # foo@...
+  hostName =      "nixos";              # ...@bar
+  hostPreset =    "desktop";             # Name of the host config to use (this one is in /laptop)
+
+  # local specifics
+  timeZone =      "Europe/Vienna";      # system time
+  kbLayout =      "de";                 # Be careful, some layouts are not supported, e.g. "at" for Austria
+  locale =        "de_AT";              # I think this one explains itself
+  searchRegion =  "AT";                 # Region for browsing purposes
+
+  # git settings
+  gitUser =       "Permafrozen";
+  gitMail =       "m8tt8o@gmail.com";
+  defaultBranch = "main";
+
+  # theme
+  wallpaper =     "space.png";          # background image, located in .../assets/wallpaper/ , also write the file ending!
+  scheme =        "eris";               # Color Theme (Chose any from the base16-schemes Package)
+
+  # hyprland
+  rounding =      "10";                 # Corner rounding
+  gaps =          "10";                 # Gaps between windows
+  shadow =        "true";               # Wheater to enable window shadow
+  opacity =       "0.7";                # background opacities for supported windows
+  blur =          "7";                  # background blur for supported windows
+  hyprConfig = ''
+    monitor= ,preferred, auto, 1 
+  '';
+
+  # default apps
+  browser =       "firefox";            # only firefox for now
+  terminal =      "kitty";              # only kitty for now
+}
