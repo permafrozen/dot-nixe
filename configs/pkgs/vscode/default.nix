8165d3402546f0750cb97eb146860e7eadbb9122
Permafrozen <m8tt8o@gmail.com>
2024-10-12 19:23:56 +0200

    updated vscode
    wrote some comments and added added config to not use ctrl-keybinds from
    vim, but from vscode.

--- configs/pkgs/vscode/default.nix
+++ configs/pkgs/vscode/default.nix
@@ -15,22 +15,28 @@
         vscodevim.vim
       ];
       userSettings = {
-        "editor.cursorBlinking"= "phase";
-        "editor.cursorSmoothCaretAnimation"= "on";
-        "editor.fontFamily"= "'Hack Nerd Font'";
-        "editor.smoothScrolling"= "true";
-        "terminal.integrated.fontFamily"= "'Hack Nerd Font'";
-        "terminal.integrated.smoothScrolling"= "true";
-        "window.titleBarStyle"= "custom";
-        "workbench.list.smoothScrolling"= "true";
-        "workbench.colorCustomizations" = {
-          "[Stylix]" = {
-            "sideBar.background"= lib.mkForce "#${config.lib.stylix.colors.base00}";
-            "activityBar.background"= lib.mkForce "#${config.lib.stylix.colors.base00}";
-            "tab.inactiveBackground"= lib.mkForce "#${config.lib.stylix.colors.base00}";
-            "editorGroupHeader.tabsBackground"= lib.mkForce "#${config.lib.stylix.colors.base00}";
+        # Editor Settigns
+          "editor.cursorBlinking"= "phase";
+          "editor.cursorSmoothCaretAnimation"= "on";
+          "editor.fontFamily"= "'Hack Nerd Font'";
+          "editor.smoothScrolling"= "true";
+          "terminal.integrated.fontFamily"= "'Hack Nerd Font'";
+          "terminal.integrated.smoothScrolling"= "true";
+          "window.titleBarStyle"= "custom";
+
+        # Theme
+          "workbench.list.smoothScrolling"= "true";
+          "workbench.colorCustomizations" = {
+            "[Stylix]" = {
+              "sideBar.background"= lib.mkForce "#${config.lib.stylix.colors.base00}";
+              "activityBar.background"= lib.mkForce "#${config.lib.stylix.colors.base00}";
+              "tab.inactiveBackground"= lib.mkForce "#${config.lib.stylix.colors.base00}";
+              "editorGroupHeader.tabsBackground"= lib.mkForce "#${config.lib.stylix.colors.base00}";
+            };
           };
-        };
+          
+        # Keybinds
+        "vim.useCtrlKeys"= false;
       };
     };
     stylix.targets.vscode.enable = true;
