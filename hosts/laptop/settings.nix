075596afd56b1791911552b03efc8208d7c67f63
Permafrozen <permafrozen@disroot.org>
2024-10-14 17:09:05 +0200

    updated laptop
    reformated

--- hosts/laptop/settings.nix
+++ hosts/laptop/settings.nix
@@ -1,14 +1,14 @@
 {
   # system settings
-  userName =      "matteo";             # foo@...
-  hostName =      "nixos";              # ...@bar
-  hostPreset =    "laptop";             # Name of the host config to use (this one is in /laptop)
+  userName =      "matteo";                       # foo@...
+  hostName =      "nixos";                        # ...@bar
+  hostPreset =    "laptop";                       # Name of the host config to use (this one is in /laptop)
 
   # local specifics
-  timeZone =      "Europe/Vienna";      # system time
-  kbLayout =      "de";                 # Be careful, some layouts are not supported, e.g. "at" for Austria
-  locale =        "de_AT";              # I think this one explains itself
-  searchRegion =  "AT";                 # Region for browsing purposes
+  timeZone =      "Europe/Vienna";                # system time
+  kbLayout =      "de";                           # Be careful, some layouts are not supported, e.g. "at" for Austria
+  locale =        "de_AT";                        # I think this one explains itself
+  searchRegion =  "AT";                           # Region for browsing purposes
 
   # git settings
   gitUser =       "Permafrozen";
@@ -17,22 +17,22 @@
 
   # theme
   wallpaper =     "foggy_mountains.png";          # background image, located in .../assets/wallpaper/ , also write the file ending!
-  scheme =        "catppuccin-frappe";               # Color Theme (Chose any from the base16-schemes Package)
+  scheme =        "catppuccin-frappe";            # Color Theme (Chose any from the base16-schemes Package)
 
   # hyprland
-  rounding =      "10";                 # Corner rounding
-  gaps =          "10";                 # Gaps between windows
-  shadow =        "false";               # Wheater to enable window shadow
-  opacity =       "0.7";                # background opacities for supported windows
-  blur =          "10";                  # background blur for supported windows
+  rounding =      "10";                           # Corner rounding
+  gaps =          "10";                           # Gaps between windows
+  shadow =        "false";                        # Wheater to enable window shadow
+  opacity =       "0.7";                          # background opacities for supported windows
+  blur =          "10";                           # background blur for supported windows
 
-  # Monitor Settings
+    # Setup Specific Hyprland Config
   hyprConfig = ''
     monitor= eDP-1, 1920x1080@60, 0x0, 1
     monitor=HDMI-A-1, 1920x1080@60, auto, 1, mirror, eDP-1
   '';
 
   # default apps
-  browser =       "firefox";            # only firefox for now
-  terminal =      "foot";              #  kitty, foot
+  browser =       "firefox";                      # only firefox for now
+  terminal =      "foot";                         #  kitty, foot
 }
