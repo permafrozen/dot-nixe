86bc0a96dda275ce2934d109acf2b86772a83535
Permafrozen <permafrozen@disroot.org>
2024-10-15 18:21:49 +0200

    added fastfetch

---
+++ configs/pkgs/fastfetch/default.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, settings }:
+
+{
+  environment.systemPackages = with pkgs; [
+    fastfetch
+  ];
+
+  home-manager.users.${settings.userName} = {
+    package = pkgs.fastfetch;
+    settings = {
+      logo = {
+        source = "~/.dot-nixe/assets/ascii-art/nixos.txt";
+        padding = {
+          right = 1;
+        };
+      };
+      display = {
+        size = {
+          binaryPrefix = "si";
+        };
+        color = "blue";
+        separator = "  ";
+      };
+      modules = [
+        {
+          type = "datetime";
+          key = "Date";
+          format = "{1}-{3}-{11}";
+        }
+        {
+          type = "datetime";
+          key = "Time";
+          format = "{14}:{17}:{20}";
+        }
+        "break"
+        "player"
+        "media"
+      ];
+    };
+  }
+}
