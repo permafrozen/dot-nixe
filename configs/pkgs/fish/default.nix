83882faa6fcee060a6413760bf86380c2cbeb827
Permafrozen <permafrozen@disroot.org>
2024-10-18 16:54:30 +0200

    updated fish config
    added a proper base16 scheme, and a function to handle not found
    commands

--- configs/pkgs/fish/default.nix
+++ configs/pkgs/fish/default.nix
@@ -5,6 +5,12 @@
     programs = {
       fish = {
         enable = true;
+        functions = {
+          __fish_command_not_found_handler = {
+            body = "__fish_default_command_not_found_handler $argv[1]";
+            onEvent = "fish_command_not_found";
+          };
+        };
         shellAliases = {
           tt = "tt -notheme";
           train = "sl -e";
@@ -12,6 +18,43 @@
           matrix = "cmatrix -s";
         };
         shellInit = ''
+          set -U fish_color_normal normal
+          set -U fish_color_command ${config.lib.stylix.colors.base0B}
+          set -U fish_color_quote ${config.lib.stylix.colors.base0A}
+          set -U fish_color_redirection ${config.lib.stylix.colors.base06}
+          set -U fish_color_end ${config.lib.stylix.colors.base0E}
+          set -U fish_color_error ${config.lib.stylix.colors.base08}
+          set -U fish_color_param ${config.lib.stylix.colors.base06}
+          set -U fish_color_comment ${config.lib.stylix.colors.base0A}
+          set -U fish_color_match ${config.lib.stylix.colors.base0D}
+          set -U fish_color_selection ${config.lib.stylix.colors.base07} --bold 
+          set -U fish_color_search_match ${config.lib.stylix.colors.base0B} --bold 
+          set -U fish_color_history_current --bold
+          set -U fish_color_operator ${config.lib.stylix.colors.base0D}
+          set -U fish_color_escape ${config.lib.stylix.colors.base0C}
+          set -U fish_color_cwd ${config.lib.stylix.colors.base0B}
+          set -U fish_color_cwd_root ${config.lib.stylix.colors.base08}
+          set -U fish_color_valid_path --underline
+          set -U fish_color_autosuggestion ${config.lib.stylix.colors.base04}
+          set -U fish_color_user ${config.lib.stylix.colors.base0B}
+          set -U fish_color_host ${config.lib.stylix.colors.base05}
+          set -U fish_color_cancel --reverse
+          set -U fish_pager_color_prefix ${config.lib.stylix.colors.base05} --bold --underline
+          set -U fish_pager_color_progress ${config.lib.stylix.colors.base07} --bold
+          set -U fish_pager_color_completion ${config.lib.stylix.colors.base05}
+          set -U fish_pager_color_description ${config.lib.stylix.colors.base0D}
+          set -U fish_pager_color_selected_background --background=${config.lib.stylix.colors.base00}
+          set -U fish_pager_color_secondary_description ${config.lib.stylix.colors.base04}
+          set -U fish_pager_color_secondary_background ${config.lib.stylix.colors.base01}
+          set -U fish_color_option ${config.lib.stylix.colors.base0E}
+          set -U fish_pager_color_background ${config.lib.stylix.colors.base00}
+          set -U fish_pager_color_secondary_completion ${config.lib.stylix.colors.base05}
+          set -U fish_pager_color_selected_completion ${config.lib.stylix.colors.base05}
+          set -U fish_pager_color_selected_prefix ${config.lib.stylix.colors.base0B}
+          set -U fish_color_host_remote ${config.lib.stylix.colors.base09}
+          set -U fish_color_keyword ${config.lib.stylix.colors.base0E}
+          set -U fish_pager_color_selected_description ${config.lib.stylix.colors.base0A}
+          set -U fish_pager_color_secondary_prefix ${config.lib.stylix.colors.base06}
           set -U fish_greeting
           cd
         '';
@@ -23,7 +66,6 @@
         shellIntegration.enableFishIntegration = true;
       };
     };
-    stylix.targets.fish.enable = true;
   };
 
   # Fish as Default Shell
