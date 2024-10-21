8776861538376cb871b5262a55d1b03a60d05d7a
Permafrozen <permafrozen@disroot.org>
2024-10-21 17:30:26 +0200

    updated ncmpcpp config
    I added a bunch of stuff, to get ncmpcpp up and running, but it still
    doesn't work. At least I know mpd is working correctly now (I think)

--- configs/pkgs/ncmpcpp/default.nix
+++ configs/pkgs/ncmpcpp/default.nix
@@ -4,7 +4,10 @@
   home-manager.users.${settings.userName} = {
     programs.ncmpcpp = {
       enable = true;
+      mpdMusicDir = "/home/${settings.userName}/Music";
       settings = {
+        mpd_host = "localhost";
+        mpd_port = "6600";
         visualizer_data_source = "/tmp/mpd.fifo";
         visualizer_output_name = "my_fifo";
         visualizer_in_stereo = "yes";
