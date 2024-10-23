c4fd3dcc3aa690a544f770ac1015a89da8506b66
Permafrozen <m8tt8o@gmail.com>
2024-10-23 08:20:42 +0200

    renamed ssh -> sshd
    Also added idle configuration for the laptop

---
+++ configs/pkgs/sshd/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  services = {
+    openssh = {
+      enable = true;
+    };
+
+    # Autologin
+    displayManager.autoLogin.enable = true;
+    displayManager.autoLogin.user = "eric";
+  };
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # don't go into sleep
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=no
+  '';
+}
--- hosts/homelab/profile.nix
+++ hosts/homelab/profile.nix
@@ -12,6 +12,6 @@
     (getPath "git")
     (getPath "syncthing")
     (getPath "fastfetch")
-    (getPath "ssh")
+    (getPath "sshd")
   ];
 }
