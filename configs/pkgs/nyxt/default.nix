2e4f3e489d0f23bb2c4be901fd3f63d98b052bdc
Permafrozen <m8tt8o@gmail.com>
2024-10-27 09:02:23 +0100

    updated nyxt config

--- configs/pkgs/nyxt/default.nix
+++ configs/pkgs/nyxt/default.nix
@@ -4,4 +4,21 @@
   environment.systemPackages = with pkgs; [
     nyxt
   ];
+
+  home-manager.users.${settings.userName} = {
+    home.file.".config/nyxt/config.lisp".text = ''
+      ;; vi keybinds
+        (define-configuration buffer
+          ((default-modes
+            (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%)))) 
+      
+      (define-configuration web-buffer
+        ((default-modes
+          (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+
+      (define-configuration web-buffer
+        ((default-modes
+          (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
+    '';
+  };
 }
