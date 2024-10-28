68368aca492eff21cb7b55406e0598ae4bf0766d
Permafrozen <permafrozen@disroot.org>
2024-10-28 13:32:27 +0100

    updated flameshot

--- configs/pkgs/flameshot/default.nix
+++ configs/pkgs/flameshot/default.nix
@@ -1,14 +1,24 @@
 { config, pkgs, settings, ...}:
 
-{
+let
+  flameshotWayland = pkgs.flameshot.overrideAttrs (oldAttrs: {
+    configureFlags = oldAttrs.configureFlags or [] ++ [ "--USE_WAYLAND_GRIM=ON" ];
+  });
+in {
+  environment.systemPackages = with pkgs; [
+    grim
+    slurp
+  ];
+
   home-manager.users.${settings.userName} = {
     services.flameshot = {
       enable = true;
+      package = flameshotWayland;
       settings = {
         General = {
           disabledTrayIcon = true;
           showStartupLaunchMessage = false;
-          savePath = "Pictures/Screenshots";
+          savePath = "/home/${settings.userName}/Pictures/Screenshots";
           saveAsFileExtension = ".png";
           uiColor = "#${config.lib.stylix.colors.base00}";
           contrastUiColor = "#${config.lib.stylix.colors.base05}";
