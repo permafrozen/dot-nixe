59f95951fc2fd78f52cff491ff6c84b4b7ab9060
Permafrozen <m8tt8o@gmail.com>
2024-11-06 07:10:01 +0100

    updated kitty
    added window margin and padding

--- configs/pkgs/kitty/default.nix
+++ configs/pkgs/kitty/default.nix
@@ -5,12 +5,20 @@
     programs.kitty = {
       enable = true;
       settings = {
+
+        # Colors
         foreground = lib.mkDefault "#${config.lib.stylix.colors.base05}";
         background = lib.mkDefault "#${config.lib.stylix.colors.base00}";
         background_opacity = lib.mkForce 1;
         background_blur = lib.mkForce 0;
+
+        # Miscellaneous
         confirm_os_window_close = "0";
         cursor_shape_unfocused = "unchanged";
+
+        # Window Margins
+        window_margin_width = 5;
+        window_padding_width = 5;
       };
     };
     stylix.targets = {
