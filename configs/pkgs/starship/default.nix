c266ddab1b8d0f4d3b1ee1bdb976f009a067b1dc
Permafrozen <m8tt8o@gmail.com>
2024-11-06 07:10:16 +0100

    updated starship config
    small changes

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -6,6 +6,11 @@
       enable = true;
       settings = {
         add_newline = true;
+        character = {
+          format = "[ ](white)";
+          success_symbol = "[ ](bold green)";
+          error_symbol = "[ ](bold red)";
+        };
       };
       enableFishIntegration = true;
     };
