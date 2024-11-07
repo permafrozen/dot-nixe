a3ec07f6ad9e36e8d581b2d648ab335c00015a01
Permafrozen <m8tt8o@gmail.com>
2024-11-07 15:47:06 +0100

    updated starship

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -12,7 +12,13 @@
           success_symbol = "[ ](bold green)";
           error_symbol = "[ ](bold red)";
         };
-
+        directory = {
+          format = "[$path]($style)[$read_only]($read_only_style) ";
+          home_symbol = "󰜥";
+          truncation_length = 5;
+          read_only = "";
+          read_only_style = "red";
+        };
       };
       enableFishIntegration = true;
     };
