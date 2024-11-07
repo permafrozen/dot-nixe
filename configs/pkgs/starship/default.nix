9ea6a6702db5a0ae3ab7473781f6815dfd358cae
Permafrozen <m8tt8o@gmail.com>
2024-11-07 13:49:49 +0100

    updated starship
    added custom pallete, wich is set by stylix. If none is set it will use
    the default.

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -5,12 +5,14 @@
     programs.starship = {
       enable = true;
       settings = {
+        palette = "custom";
         add_newline = true;
         character = {
           format = "[ ](white)";
           success_symbol = "[ ](bold green)";
           error_symbol = "[ ](bold red)";
         };
+
       };
       enableFishIntegration = true;
     };
