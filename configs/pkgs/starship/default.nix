2cfe8113d2ca06bd2ae0e907ea79e7ea3565b2a8
Permafrozen <m8tt8o@gmail.com>
2024-11-08 13:24:09 +0100

    updated starship

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -5,13 +5,19 @@
     programs.starship = {
       enable = true;
       settings = {
+
+        # Misc
         palette = "custom";
         add_newline = true;
+
+        # Prompt Character
         character = {
           format = "[ ](white)";
           success_symbol = "[ ](bold green)";
           error_symbol = "[ ](bold red)";
         };
+
+        # Directory Module
         directory = {
           format = "[$path]($style)[$read_only]($read_only_style) ";
           home_symbol = " ";
@@ -19,6 +25,8 @@
           read_only = " ";
           read_only_style = "red";
         };
+
+        # Shell Module
         shell = {
           fish_indicator = "fsh";
           bash_indicator = "bsh";
@@ -27,7 +35,17 @@
           style = "yellow bold";
           disabled = true;
         };
+
+        # Nix-Shell Module
+        nix_shell = {
+          format = "via [☃️ $state( \($name\))](bold blue) ";
+          impure_msg = "[impure shell](bold red)";
+          pure_msg = "[pure shell](bold green)";
+          unknown_msg = "[unknown shell](bold yellow)";
+        };
       };
+
+      # Shell Integration
       enableFishIntegration = true;
     };
   };
