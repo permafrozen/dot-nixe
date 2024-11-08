0a347c5561f4c08fb565add084d60293642c774c
Permafrozen <m8tt8o@gmail.com>
2024-11-08 13:04:45 +0100

    added starship config

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -8,16 +8,25 @@
         palette = "custom";
         add_newline = true;
         character = {
-          format = "[ ](white)";
-          success_symbol = "[ ](bold green)";
-          error_symbol = "[ ](bold red)";
+          format = "[ ](white)";
+          success_symbol = "[ ](bold green)";
+          error_symbol = "[ ](bold red)";
         };
         directory = {
           format = "[$path]($style)[$read_only]($read_only_style) ";
-          home_symbol = "󰜥";
+          home_symbol = " ";
           truncation_length = 5;
-          read_only = "";
+          read_only = " ";
           read_only_style = "red";
+        };
+        shell = {
+          fish_indicator = "󰈺 ";
+          bash_indicator = "󱆃 ";
+          zsh_indicator = "zsh";
+          powershell_indicator = "󰨊";
+          unknown_indicator = "?";
+          style = "yellow bold";
+          disabled = false;
         };
       };
       enableFishIntegration = true;
