7268b76ccdba1289e92526822e6daac757ff6073
Permafrozen <m8tt8o@gmail.com>
2024-11-14 20:10:16 +0100

    updated ags config
    moved imports to home.nix due to build errors. Also added minimal
    configuration.

--- configs/pkgs/ags/default.nix
+++ configs/pkgs/ags/default.nix
@@ -1,11 +1,19 @@
-{ settings, inputs, ... }:
+{ settings, ... }:
 
 {
-  imports = [ inputs.ags.homeManagerModules.default ];
 
   home-manager.users.${settings.userName} = {
     programs.ags = {
       enable = true;
+
+      # ags config dir
+      configDir = ./. + "project/app.ts";
+
+      # additional packages to add to gjs's runtime
+      # extraPackages = with pkgs; [
+      #   inputs.ags.packages.${pkgs.system}.battery
+      #   fzf
+      # ];
     };
   };
 }
