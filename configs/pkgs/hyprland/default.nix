bf9af2b9dc5a45b2739aee0f28729fae570b25ef
Permafrozen <m8tt8o@gmail.com>
2024-11-15 06:20:33 +0100

    updated hyprland config
    removed unnecessary comments

--- configs/pkgs/hyprland/default.nix
+++ configs/pkgs/hyprland/default.nix
@@ -139,7 +139,6 @@
           # TODO
 
           # Screenshot keybinds
-          # "$altMod, PRINT, exec, grim - | satty --filename -"
           "$altMod, PRINT, exec, grim -g \"$(slurp -o -c '##ff0000ff')\" -t png - | satty --filename -"
           "$mainMod, PRINT, exec, grim - | wl-copy --type image/png"
 
