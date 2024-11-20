47ededec54abae5bd16a3ea82f14ec7536c322c3
Permafrozen <m8tt8o@gmail.com>
2024-11-20 21:43:35 +0100

    greeted that d

--- configs/pkgs/greetd/default.nix
+++ configs/pkgs/greetd/default.nix
@@ -1,5 +1,16 @@
-{ ... }:
+{ pkgs, settings, ... }:
 
 {
-  services.greetd.enable = true;
+  environment.systemPackages = with pkgs; [ greetd.greetd ];
+
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "${settings.userName}";
+      };
+      default_session = initial_session;
+    };
+  };
 }
