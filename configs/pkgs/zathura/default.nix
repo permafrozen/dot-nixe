6ecb0069107561be7fe4e34c6070839161a5c1f9
Permafrozen <m8tt8o@gmail.com>
2024-11-20 06:55:17 +0100

    updated zathura config
    fixxed oopsi woopsi

--- configs/pkgs/zathura/default.nix
+++ configs/pkgs/zathura/default.nix
@@ -7,7 +7,8 @@
       package = pkgs.zathura;
       options = {
         default-bg = lib.mkForce
-          "rgba(${config.lib.stylix.colors.base00-hex-r}, ${config.lib.stylix.colors.base00-hex-g}, ${config.lib.stylix.colors.base00-hex-b}, 0,5)";
+          "rgba(${config.lib.stylix.colors.base00-hex-r}, ${config.lib.stylix.colors.base00-hex-g}, ${config.lib.stylix.colors.base00-hex-b}, 0.5)";
+
       };
     };
   };
