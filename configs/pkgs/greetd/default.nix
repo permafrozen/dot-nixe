45dd76aaa016bbc8552b179824670d17da6a2f50
Permafrozen <m8tt8o@gmail.com>
2024-11-21 07:55:58 +0100

    updated greetd config
    changed to tui greet + added kernel parameters to fix systemd logs
    overwriting the tui greet login screen.

--- configs/pkgs/greetd/default.nix
+++ configs/pkgs/greetd/default.nix
@@ -1,13 +1,19 @@
 { pkgs, settings, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ greetd.greetd ];
+  environment.systemPackages = with pkgs.greetd; [ greetd tuigreet ];
 
+  # fix greetd being overwritten by systemd log
+  boot.kernelParams = [ "console=tty2" ];
+  services.greetd.vt = 1;
+
+  # tuigreet config
   services.greetd = {
     enable = true;
     settings = rec {
       initial_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland";
+        command =
+          "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
         user = "${settings.userName}";
       };
       default_session = initial_session;
