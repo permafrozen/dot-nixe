f85084152ca1bc6c8dfe657a35f29afba0660ff5
Permafrozen <m8tt8o@gmail.com>
2024-11-22 22:35:11 +0100

    updated starship config
    added custom format, disabled some parts

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -7,42 +7,44 @@
       settings = {
 
         # Misc
+        format = ''
+          $directory$character
+        '';
         palette = "custom";
-        add_newline = true;
+        add_newline = false;
 
         # Prompt Character
         character = {
-          format = "[  ](white)";
-          success_symbol = "[  ](bold green)";
+          success_symbol = "[ ](bold white)";
           error_symbol = "[ ](bold red)";
         };
 
         # Directory Module
         directory = {
           format = "[$path]($style)[$read_only]($read_only_style) ";
-          home_symbol = " ";
+          home_symbol = "~";
           truncation_length = 5;
-          read_only = " ";
+          read_only = "";
           read_only_style = "red";
         };
 
         # Shell Module
-        shell = {
-          fish_indicator = "fsh";
-          bash_indicator = "bsh";
-          zsh_indicator = "zsh";
-          unknown_indicator = "?  ";
-          style = "yellow bold";
-          disabled = true;
-        };
+        # shell = {
+        #   fish_indicator = "fsh";
+        #   bash_indicator = "bsh";
+        #   zsh_indicator = "zsh";
+        #   unknown_indicator = "?  ";
+        #   style = "yellow bold";
+        #   disabled = true;
+        # };
 
         # Nix-Shell Module
-        nix_shell = {
-          format = "via [󰜗 $state( ($name))](bold blue) ";
-          impure_msg = "[impure](bold red)";
-          pure_msg = "[pure](bold green)";
-          unknown_msg = "[unknown shell](bold yellow)";
-        };
+        # nix_shell = {
+        #   format = "via [󰜗 $state( ($name))](bold blue) ";
+        #   impure_msg = "[impure](bold red)";
+        #   pure_msg = "[pure](bold green)";
+        #   unknown_msg = "[unknown shell](bold yellow)";
+        # };
       };
 
       # Shell Integration
