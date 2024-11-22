2a461e02ac33634090778e5a8a47852f79c45f60
Permafrozen <m8tt8o@gmail.com>
2024-11-22 22:34:39 +0100

    updated foot config
    removed annoying settings

--- configs/pkgs/foot/default.nix
+++ configs/pkgs/foot/default.nix
@@ -9,11 +9,11 @@
       server.enable = false;
       settings = {
         main = {
-          pad = "18x18 center";
-          vertical-letter-offset = "-4px";
-          line-height = "25px";
-          letter-spacing = "-1px";
-          bold-text-in-bright = "palette-based";
+          # pad = "18x18 center";
+          # vertical-letter-offset = "-4px";
+          # line-height = "25px";
+          # letter-spacing = "-1px";
+          # bold-text-in-bright = "palette-based";
           resize-delay-ms = "80";
         };
 
