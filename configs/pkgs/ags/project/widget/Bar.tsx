9833531dda121c1c17b4e883d0c2f6499cbe89d7
Permafrozen <m8tt8o@gmail.com>
2024-11-23 23:56:27 +0100

    updated ags config
    changed to display the initialClass of the focused client instead of the
    title, because I prefer just seing the name of the app. Also updated css
    to be correct.

--- configs/pkgs/ags/project/widget/Bar.tsx
+++ configs/pkgs/ags/project/widget/Bar.tsx
@@ -140,7 +140,8 @@ function FocusedClient() {
   return (
     <box className="Focused" visible={focused.as(Boolean)}>
       {focused.as(
-        (client) => client && <label label={bind(client, "title").as(String)} />
+        
+        (client) => client && <label label={bind(client,"initialClass").as(String)} />
       )}
     </box>
   );
