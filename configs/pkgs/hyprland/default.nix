a0e8b91788c6639bd8d03ab8107eaf3068561a1e
Permafrozen <m8tt8o@gmail.com>
2024-12-08 13:44:02 +0100

    changed hyprland border color

--- configs/pkgs/hyprland/default.nix
+++ configs/pkgs/hyprland/default.nix
@@ -54,7 +54,7 @@
 
         general = {
           border_size = "2";
-          "col.active_border" = "0xff${config.lib.stylix.colors.base0D}";
+          "col.active_border" = "0x4D${config.lib.stylix.colors.base05}";
           "col.inactive_border" = "0x00${config.lib.stylix.colors.base03}";
           resize_on_border = "true";
 
