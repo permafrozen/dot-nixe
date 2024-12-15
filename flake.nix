fadfc9d1319527223ca692269f8edd43ae89f954
Permafrozen <m8tt8o@gmail.com>
2024-12-15 09:33:15 +0100

    flake.nix: fixed small typo

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     hypr-dynamic-cursors = {
       url = "github:VirtCode/hypr-dynamic-cursors";
       inputs.hyprland.follows = "hyprland";
-      # inputs.nixpkgs.follows = "nixkpgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
