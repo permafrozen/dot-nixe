fb5c91b4038856115b73ab53b9bc893daa5452bc
Permafrozen <m8tt8o@gmail.com>
2025-01-09 09:32:27 +0100

    added Ghostty configuration

--- configs/pkgs/ghostty/default.nix
+++ configs/pkgs/ghostty/default.nix
@@ -1,5 +1,23 @@
-{ pkgs, ... }:
+{ config, pkgs, settings, ... }:
 
 {
   environment.systemPackages = [ pkgs.ghostty ];
+
+  home-manager.users.${settings.userName} = {
+    home.file.".config/ghostty/config".text = ''
+      background = ${config.lib.stylix.colors.base00}
+      background-opacity = ${settings.opacity}
+      foreground = ${config.lib.stylix.colors.base05} 
+
+      window-titlebar-background = ${config.lib.stylix.colors.base00}
+      window-titlebar-foreground = ${config.lib.stylix.colors.base05}
+
+      resize-overlay = never
+      gtk-titlebar = false
+      confirm-close-surface = false
+
+      cursor-style = bar
+      cursor-style-blink = true
+    '';
+  };
 }
