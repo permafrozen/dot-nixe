1d7b5809ae1bc36d408b998e6f7f30d89768429e
Permafrozen <m8tt8o@gmail.com>
2025-01-09 09:32:06 +0100

    added docker pkg

--- configs/pkgs/docker/default.nix
+++ configs/pkgs/docker/default.nix
@@ -1,6 +1,11 @@
-{ settings, ... }:
+{ pkgs, settings, ... }:
 
 {
   virtualisation.docker.enable = true;
   users.users.${settings.userName}.extraGroups = [ "docker" ];
+
+  environment.systemPackages = [ pkgs.mysql-workbench pkgs.jetbrains.datagrip];
+
+  # Open Firewall for Connections
+  networking.firewall.allowedTCPPorts = [ 3306 ];
 }
