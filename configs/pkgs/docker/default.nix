1f21f7262318213ca5ae98a9cca0e3164b768b80
Permafrozen <m8tt8o@gmail.com>
2025-01-09 12:50:15 +0100

    formating patch

--- configs/pkgs/docker/default.nix
+++ configs/pkgs/docker/default.nix
@@ -4,7 +4,7 @@
   virtualisation.docker.enable = true;
   users.users.${settings.userName}.extraGroups = [ "docker" ];
 
-  environment.systemPackages = [ pkgs.mysql-workbench pkgs.jetbrains.datagrip];
+  environment.systemPackages = [ pkgs.mysql-workbench pkgs.jetbrains.datagrip ];
 
   # Open Firewall for Connections
   networking.firewall.allowedTCPPorts = [ 3306 ];
