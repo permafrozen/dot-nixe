2f895881e82b2e03ac27e34ab179592b66513663
Permafrozen <m8tt8o@gmail.com>
2025-01-15 15:39:47 +0100

    improved zed config

--- configs/pkgs/zed/default.nix
+++ configs/pkgs/zed/default.nix
@@ -1,6 +1,9 @@
-{ pkgs, settings, ... }:
+{ pkgs, settings, lib, ... }:
 
-{
+let
+  ui_font_size = (lib.toInt settings.font-size) * 5 / 3;
+  buffer_font_size = (lib.toInt settings.font-size) * 4 / 3;
+in {
   imports = [ ./theme.nix ];
   home-manager.users.${settings.userName} = {
     programs.zed-editor = {
@@ -10,12 +13,19 @@
       package = pkgs.zed-editor;
       userKeymaps = { };
       userSettings = {
+        vim_mode = true;
+
+        # Font
+        inherit ui_font_size;     # pt -> px
+        inherit buffer_font_size; # ^
+        buffer_font_family = "Hack Nerd Font";
+
+        # Theme
         theme = {
           mode = "light";
           light = "Stylix";
           dark = "Stylix";
         };
-        vim_mode = true;
       };
     };
   };
