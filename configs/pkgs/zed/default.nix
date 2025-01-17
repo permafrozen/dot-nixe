91ed4e29a319f1bcd9e827fc539753dc9cd836b1
Permafrozen <m8tt8o@gmail.com>
2025-01-17 13:23:46 +0100

    made zed good

--- configs/pkgs/zed/default.nix
+++ configs/pkgs/zed/default.nix
@@ -8,24 +8,52 @@ in {
   home-manager.users.${settings.userName} = {
     programs.zed-editor = {
       enable = true;
-      extensions = [ ];
-      extraPackages = [ ];
+      extensions = [
+        "csharp"
+        "java"
+        "lua"
+        "zig"
+        "nix"
+        "dockerfile"
+        "docker-compose"
+        "svelte"
+        "scss"
+        "log"
+        "elixir"
+        "env"
+        "latex"
+        "ini"
+        "make"
+        "xml"
+      ];
+      extraPackages = [ pkgs.nixd ];
       package = pkgs.zed-editor;
       userKeymaps = { };
       userSettings = {
         vim_mode = true;
 
+        features = { copilot = false; };
+
+        # Telemetry
+        telemetry = {
+          diagnostics = false;
+          metrics = false;
+        };
+
         # Font
-        inherit ui_font_size;     # pt -> px
+        inherit ui_font_size; # pt -> px
         inherit buffer_font_size; # ^
         buffer_font_family = "Hack Nerd Font";
 
         # Theme
         theme = {
-          mode = "light";
-          light = "Stylix";
-          dark = "Stylix";
+          mode = "dark";
+          light = "Base16 Stylix";
+          dark = "Base16 Stylix";
         };
+
+        # LSP - Config
+        lsp = { nix = { binary.path = lib.getExe pkgs.nixd; }; };
       };
     };
   };
