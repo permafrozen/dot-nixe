7b456dbca8e1871c91c9a037ba31b9fb3cd39737
Permafrozen <m8tt8o@gmail.com>
2025-01-21 09:32:44 +0100

    moved datagrip package

--- configs/pkgs/docker/default.nix
+++ configs/pkgs/docker/default.nix
@@ -1,10 +1,8 @@
-{ pkgs, settings, ... }:
+{ settings, ... }:
 
 {
   virtualisation.docker.enable = true;
   users.users.${settings.userName}.extraGroups = [ "docker" ];
-
-  environment.systemPackages = [ pkgs.mysql-workbench pkgs.jetbrains.datagrip ];
 
   # Open Firewall for Connections
   networking.firewall.allowedTCPPorts = [ 3306 ];
