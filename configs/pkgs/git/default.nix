cb0d42effa10d9f42780f1f21f92adc887dc4f15
Permafrozen <m8tt8o@gmail.com>
2025-02-01 21:16:37 +0100

    moved lazygit and added gh-cli
    wanted to get all git related stuff to one place

--- configs/pkgs/git/default.nix
+++ configs/pkgs/git/default.nix
@@ -2,11 +2,24 @@
 
 {
   home-manager.users.${settings.userName} = {
-    programs.git = {
-      enable = true;
-      userName = settings.gitUser;
-      userEmail = settings.gitMail;
-      extraConfig = { init.defaultBranch = settings.defaultBranch; };
+    programs = {
+      git = {
+        enable = true;
+        userName = settings.gitUser;
+        userEmail = settings.gitMail;
+        extraConfig = { init.defaultBranch = settings.defaultBranch; };
+      };
+      gh = {
+        enable = true;
+        git_protocol = "ssh";
+        aliases = {
+          #TODO
+        };
+      };
+      lazygit = {
+        enable = true;
+        settings = { };
+      };
     };
   };
 }
