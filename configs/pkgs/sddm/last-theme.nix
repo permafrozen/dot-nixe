0171e1fc38dbefcd25f9c278ef363bd6abd2e22f
Permafrozen <m8tt8o@gmail.com>
2025-02-01 22:50:48 +0100

    created package for sddm theme
    wrote a package for my own sddm theme wich fetches it from github.

---
+++ configs/pkgs/sddm/last-theme.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchFromGitHub }:
+
+{
+  stdenv.mkDerivation = {
+    pname = "last-theme";
+    version = "1.0.0";
+
+    src = fetchFromGitHub {
+      owner = "Permafrozen";
+      repo = "sddm-theme";
+      rev = "ba91974e91431dbb8c0e4a20bfed4cd1a9c33e03";
+      hash = "sha256-aUT/TPm6KT2g1qK6RgdlNKvaYCPH3wrxbWZ9Ty4jUMI=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -r . $out/share/sddm/themes/last-theme
+    '';
+  };
+}
