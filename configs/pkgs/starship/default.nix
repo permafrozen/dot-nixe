46b4c285b8e8b11805136f67300e3fcea49a2f56
Permafrozen <m8tt8o@gmail.com>
2025-03-20 16:25:42 +0100

    fixed starship configuration to display all modules

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -7,16 +7,17 @@
       settings = {
 
         # Misc
-        # format = ''
-        #   $directory$character
-        # '';
+        format = ''
+          $directory$all$character
+        '';
+
         palette = "custom";
         add_newline = false;
 
         # Prompt Character
         character = {
-          success_symbol = "[ > ](bold white)";
-          error_symbol = "[!> ](bold red)";
+          success_symbol = "[  ](bold white)";
+          error_symbol = "[  ](bold red)";
         };
 
         # Directory Module
@@ -27,28 +28,7 @@
           read_only = "";
           read_only_style = "red";
         };
-
-        # Shell Module
-        # shell = {
-        #   fish_indicator = "fsh";
-        #   bash_indicator = "bsh";
-        #   zsh_indicator = "zsh";
-        #   unknown_indicator = "?  ";
-        #   style = "yellow bold";
-        #   disabled = true;
-        # };
-
-        # Nix-Shell Module
-        # nix_shell = {
-        #   format = "via [󰜗 $state( ($name))](bold blue) ";
-        #   impure_msg = "[impure](bold red)";
-        #   pure_msg = "[pure](bold green)";
-        #   unknown_msg = "[unknown shell](bold yellow)";
-        # };
       };
-
-      # Shell Integration
-      enableFishIntegration = true;
     };
   };
 }
