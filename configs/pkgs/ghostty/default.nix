66cb6a8db927e0eead7a4aa607047361bbf6e927
Permafrozen <m8tt8o@gmail.com>
2025-03-23 18:31:23 +0100

    replaced ghostty config with proper home-manager module

--- configs/pkgs/ghostty/default.nix
+++ configs/pkgs/ghostty/default.nix
@@ -4,23 +4,23 @@
   environment.systemPackages = [ pkgs.ghostty ];
 
   home-manager.users.${settings.userName} = {
-    home.file.".config/ghostty/config".text = ''
-      background = ${config.lib.stylix.colors.base00}
-      background-opacity = ${settings.opacity}
-      foreground = ${config.lib.stylix.colors.base05} 
-
-      window-titlebar-background = ${config.lib.stylix.colors.base00}
-      window-titlebar-foreground = ${config.lib.stylix.colors.base05}
-
-      resize-overlay = never
-      gtk-titlebar = false
-      confirm-close-surface = false
-
-      cursor-style = bar
-      cursor-style-blink = true
-      font-size = ${settings.font-size}
-
-      window-padding-x = 10
-    '';
+    programs.ghostty = {
+      enable = true;
+      clearDefaultKeybinds = true;
+      settings = {
+        foreground = "${config.lib.stylix.colors.base05}";
+        background = "${config.lib.stylix.colors.base00}";
+        background-opacity = "${settings.opacity}";
+        window-titlebar-background = "${config.lib.stylix.colors.base00}";
+        window-titlebar-foreground = "${config.lib.stylix.colors.base05}";
+        resize-overlay = "never";
+        gtk-titlebar = false;
+        confirm-close-surface = false;
+        cursor-style = "bar";
+        cursor-style-blink = true;
+        font-size = "${settings.font-size}";
+        window-padding-x = 10;
+      };
+    };
   };
 }
