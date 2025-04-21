f493404599ca337796b8b31b2cea7d0653f849df
Permafrozen <m8tt8o@gmail.com>
2025-04-21 16:30:08 +0200

    updated starship config
    overhauled the whole configuration to better suit my needs

--- configs/pkgs/starship/default.nix
+++ configs/pkgs/starship/default.nix
@@ -1,32 +1,52 @@
-{ settings, ... }:
-
-{
+{ settings, ... }: {
   home-manager.users.${settings.userName} = {
     programs.starship = {
       enable = true;
       settings = {
-
-        # Misc
-        format = ''
-          $directory$all$character
-        '';
-
-        palette = "custom";
+        "$schema" = "https://starship.rs/config-schema.json";
         add_newline = false;
-
-        # Prompt Character
         character = {
-          success_symbol = "[  ](bold white)";
-          error_symbol = "[  ](bold red)";
+          error_symbol = "[>](bold red)";
+          format = "$symbol ";
+          success_symbol = "[>](bold)";
         };
-
-        # Directory Module
         directory = {
+          fish_style_pwd_dir_length = 1;
           format = "[$path]($style)[$read_only]($read_only_style) ";
-          home_symbol = "~";
-          truncation_length = 5;
-          read_only = "";
+          read_only = " ";
           read_only_style = "red";
+          style = "bold path";
+        };
+        format = "$username$hostname$directory$all$character";
+        git_branch = {
+          format = "[on](italic) [$symbol$branch(:$remote_branch)]($style) ";
+          style = "bold information";
+        };
+        git_status = {
+          format = "([\\[$all_status$ahead_behind\\]]($style) )";
+          modified = "!$count";
+          style = "bold status";
+        };
+        hostname = {
+          format = "[$ssh_symbol$hostname]($style) [in](italic) ";
+          ssh_only = false;
+          style = "bold username";
+        };
+        line_break = { disabled = true; };
+        palette = "custom";
+        palettes = {
+          custom = {
+            information = "purple";
+            path = "blue";
+            status = "red";
+            username = "cyan";
+          };
+        };
+        username = {
+          format = "[$user@]($style)";
+          show_always = true;
+          style_root = "red bold";
+          style_user = "bold username";
         };
       };
     };
